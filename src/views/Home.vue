31c622ddbcf4288ae70e726eb3e06ff0ddb749e1
朱永杰 <1668584296@qq.com>
2022-08-17 10:49:02 +0800

    项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="title">首页</div>
+  <HelloWorld msg="大房间"></HelloWorld>
+</template>
+
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+import { ref } from "vue";
+const color = ref("red");
+</script>
+
+<style lang="scss" scoped>
+.title {
+	font-size: 30px;
+	color: v-bind(color);
+}
+</style>
